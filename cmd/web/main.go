158074de0cd4d50273f7556fb79670c6697fbd8f
Sid Patel <sid.auburn@gmail.com>
2024-09-11 14:03:25 -0400

    using command line flags to assign the values at run time

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,13 @@
 package main
+
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 func main() {
+	addr:=flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 	mux := http.NewServeMux()
 	staticfileServer := http.FileServer(http.Dir("./ui/static/"))
 
@@ -14,8 +18,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
