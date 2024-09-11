c9e4c98b7dbfba3f63533cf461cd0b37f2e3811a
Sid Patel <sid.auburn@gmail.com>
2024-09-11 14:13:25 -0400

    wip

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,22 @@ import (
 	"log"
 	"net/http"
 )
+
+type config struct {
+	addr string
+	staticDir string
+}
+
 func main() {
-	addr:=flag.String("addr", ":4000", "HTTP network address")
+	// a new config struct
+	var config config
+	// define and parse command line flags to get the runtime values
+	flag.StringVar(&config.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&config.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
+	
 	mux := http.NewServeMux()
-	staticfileServer := http.FileServer(http.Dir("./ui/static/"))
+	staticfileServer := http.FileServer(http.Dir(config.staticDir))
 
 	// Staitc file server route
 	mux.Handle("/static/", http.StripPrefix("/static", staticfileServer))
@@ -19,7 +30,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	log.Printf("Starting server on %s", config.addr)
+	err := http.ListenAndServe(config.addr, mux)
 	log.Fatal(err)
 }
