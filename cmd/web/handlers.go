ffc66705aa6dddb5f0778cfe8e6867c14be31102
Siddharth Patel <siddharthpatel@Siddharths-MacBook-Air.local>
2024-09-10 21:06:33 -0400

    created sample html template that the handlers can serve

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,13 +1,32 @@
 package main
+
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-	http.NotFound(w, r)
-	return
+		http.NotFound(w, r)
+		return
+	}
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/navbar.html",
+		"./ui/html/pages/home.html",
+		}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base",nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
