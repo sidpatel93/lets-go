6850735dd449e11b1781ab8c1794443fc5cba42d
Sid Patel <sid.auburn@gmail.com>
2024-09-11 14:20:05 -0400

    using custom loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 type config struct {
@@ -18,7 +19,11 @@ func main() {
 	flag.StringVar(&config.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&config.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
-	
+
+	// custom loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	staticfileServer := http.FileServer(http.Dir(config.staticDir))
 
@@ -30,7 +35,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", config.addr)
+	infoLog.Printf("Starting server on %s", config.addr)
 	err := http.ListenAndServe(config.addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
