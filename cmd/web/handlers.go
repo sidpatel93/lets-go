d76c9754be0b334fbb343c904c90fbaa826dba55
Siddharth Patel <sid.auburn@gmail.com>
2024-09-13 20:39:13 -0400

    using the db connection to insert the new record in db

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -43,5 +43,16 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa" 
+	expires := 7
+	// Pass the data to the SnippetModel.Insert() method, receiving the // ID of the new record back.
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 	w.Write([]byte("Create a new snippet..."))
 }
